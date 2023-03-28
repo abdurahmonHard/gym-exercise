bf8e199f71c992f4faab969dd254a912eeff7732
Abdurahmon <abdurahmonusmonov633@gmail.com>
2023-03-29 03:50:06 +0900

    four-commit

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -12,9 +12,10 @@ export const youtubeOptions = {
     method: 'GET',
     headers: {
         'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-        'X-RapidAPI-Key': '522cc987f1mshe5f9f993899b784p11834djsndc95c970787e',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
     },
 };
+
 
 export const fetchData = async (url, options)=>{
     const response = await fetch(url, options);
